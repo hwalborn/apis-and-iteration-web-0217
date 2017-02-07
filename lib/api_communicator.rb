45f91bf250186127fc29c5ff8b288fa3103860bd
hwalborn <hwalborn@gmail.com>
2017-02-07 16:37:45 -0500

    may the force be with you

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,23 +3,48 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  result_length = 2
+  while result_length != 1
+    character_hash = get_and_parse('https://swapi.co/api/people/?search=' + character)
+    film_api_arr = find_char_info(character_hash)
+    result_length = character_hash["count"]
+    if (result_length > 1)
+        puts "More than one result, try again. Type exit to exit out of the program."
+        character = gets.chomp.downcase
+    elsif character == "exit"
+        exit
+    elsif (result_length < 1)
+        puts "No results found. Try again. Type exit to exit out of the program."
+        character = gets.chomp.downcase
+    end
+  end
+
+  #Iterate through API to make the urls Hashes
+  film_api_arr.map!{|url|
+    url = get_and_parse(url)
+  }
+
 end
 
+#make web requests
+def get_and_parse(api)
+  gotten = RestClient.get(api)
+  return JSON.parse(gotten)
+end
+
+#return film array
+def find_char_info(char_hash)
+  char_hash['results'][0]['films']
+end
+
+
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  return_arr = []
+  films_hash.each_with_index {|film, index|
+    return_arr << "#{index + 1} #{film['title']}"
+  }
+
+  return_arr.each{|film| puts film}
 end
 
 def show_character_movies(character)
@@ -27,6 +52,7 @@ def show_character_movies(character)
   parse_character_movies(films_hash)
 end
 
+#get_character_movies_from_api("Luke Skywalker")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
